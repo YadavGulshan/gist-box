db158a8c65ab0f131590edf027b9dee55e8f0824
YadavGulshan <yadavgulshan542@gmail.com>
2022-02-26 13:01:50 +0530

    added auth through the token

--- index.ts
+++ index.ts
@@ -12,3 +12,26 @@ const {
 const octokit = new Octokit({
   auth: `token ${githubToken}`,
 });
+
+async function formatBytes(props: { bytes: number | 0; decimals: number | 0 }) {
+  if (props.bytes == 0) return "0 bytes";
+  const k = 1024;
+  const dm = props.decimals < 0 ? 0 : props.decimals;
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+  const i = Math.floor(Math.log(props.bytes) / Math.log(k));
+  return (
+    parseFloat((props.bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i]
+  );
+}
+
+async function main() {
+  const arr = [];
+  const isPrivate = considerPrivate === "true";
+  const userDataRaw = await octokit.users.getAuthenticated();
+  const userData = userDataRaw.data;
+  console.log(`${userData.login} (${userData.name})`);
+}
+
+(async () => {
+  await main();
+})();
