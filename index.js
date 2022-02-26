1911f521bb2922c93d495b3c14e3fc2b9183372c
YadavGulshan <yadavgulshan542@gmail.com>
2022-02-26 14:27:26 +0530

    All done!

--- index.js
+++ index.js
@@ -47,13 +47,92 @@ async function main() {
     });
     console.log(`Disk Usage: ${diskUsage}`);
   }
-  // Append the public repos
-  arr.push(userData.public_repos);
+  // If isPrivate is true then append total no. private and public repos too into the array
+  // else only append public repos
   isPrivate
-    ? userData.owned_private_repos != null
-      ? arr.push(userData.owned_private_repos)
+    ? userData.owned_private_repos != undefined
+      ? arr.push(userData.public_repos + userData.owned_private_repos)
       : arr.push(0)
     : arr.push(userData.public_repos);
+  // Append the total no. of contributions in current year
+  arr.push(contributionData.data.years[0].total);
+  // Append the year
+  arr.push(contributionData.data.years[0].year);
+  // Adding into the array, if user is looking for a job or not.
+  userData.hireable
+    ? arr.push("💼 Looking for a job")
+    : arr.push("💼 Not looking for a job");
+  // Append the no. of public gists
+  arr.push(userData.public_gists);
+  // Append the no. of public keys
+  arr.push(pkDataRaw.data.length);
+  await updateGist(arr);
+}
+async function updateGist(data) {
+  let gist;
+  /// iF gist id is present then set the gist variable
+  // else end the program
+  if (gistId != undefined) {
+    try {
+      gist = await octokit.gists.get({
+        gist_id: gistId,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  } else return;
+  const lines = [];
+  // number of contributions in year
+  const contributionPoint = [
+    `🏆 ${humanize(data[2])} contributions in ${data[3]}`,
+  ];
+  // If directely appended, the [line] array will be like,
+  // [['data']]
+  // So, we need to flatten it
+  //
+  // We can use for each loop...
+  // ```
+  // contributionPoint.forEach((line) => {
+  //     lines.push(line);
+  // });
+  // ```
+  // Or this way,
+  lines.push(contributionPoint.join(""));
+  // disk usage
+  const totalDiskUsage = [`📦 Used ${data[0]} in GitHub's Storage`];
+  lines.push(totalDiskUsage.join(" "));
+  // public gists
+  const publicGists = [`📜 ${data[5]} Public Gists`];
+  lines.push(publicGists.join(" "));
+  // public keys
+  const publicKeys = [`🔑 ${data[6]} Public Keys`];
+  lines.push(publicKeys.join(" "));
+  // Is Hireable?
+  const isHireable = [data[4]];
+  lines.push(isHireable.join(" "));
+  // Adding the [lines] array into the gist file
+  if (lines.length != 0) {
+    try {
+      console.log(lines.join("\n"));
+      const filename =
+        gist != undefined
+          ? gist.data.files != undefined
+            ? Object.keys(gist.data.files)[0]
+            : "File not found"
+          : "";
+      await octokit.gists.update({
+        gist_id: gistId,
+        files: {
+          [filename]: {
+            filename: `🐱 GitHub Data`,
+            content: lines.join("\n"),
+          },
+        },
+      });
+    } catch (err) {
+      console.error(`Unable to update gist\n${err}`);
+    }
+  }
 }
 (async () => {
   await main();
